7ac7be7ef24dd535565973056919eb6ef5121fa9
Notaera <53047982+Notaera@users.noreply.github.com>
2020-10-07 23:22:57 -0700

    Update corsa.js

--- corsa.js
+++ corsa.js
@@ -10,9 +10,7 @@ function BoRayMaa(){
 	LoT = window.innerHeight;
 	window.resizeTo(ScreenWidth,ScreenHeight);
 	
-	document.addEventListener("keypress", function(e) {
-	    BeteverePlay(e.keyCode);
-	});
+
 	MaaAboutMe();
 //	verever();
 	Lotani();
@@ -20,51 +18,20 @@ function BoRayMaa(){
 }
 
 function MaaAboutMe(){
-	var Notaeram = document.createElement("p");
-	var Notaeram2 = document.createElement("p");
-	var Notaeram3 = document.createElement("p");
-	var Notaeram4 = document.createElement("p");
 	var Notaeram5 = document.createElement("p");
 	var Notaeram6 = document.createElement("p");
-var Notaera = document.createTextNode(`hi
-im just rushing up a site because i think it more polite than spamming a bazillion words into messenger.
-First, a little about me: I'm a horse from space.
-I have some node.js, javascript, java, and android programming experience and did things with the waves.api and cryptocurrencies for an experimental community.  I have a good perspective of what is doable and how easy it is on computery things.  But I'm not a worker.  I dash thru a place and pick up the knowledge and to the next thing.  I wrote my first sheet music for orchestra in elementary school and progress continually ~I can make the audio addictingly appealing and very high quality.  I am also able to type words of very high quality and may type influencial books explaining our government direction if we progress.`);
-
-var Notaera2 = document.createTextNode(`
-i believe the roles of citizens in the government is due for an upgrade.  Technology has grown lots since the system was set up.  I believe the best, highest budget, beautiful, and intuitive forums should be government forums designed to facillitate even the youngest children to be able to get a thought all the way up to those who can make it happen if it's high quality enough.  Social sway not the requirement that makes something seen.  'Common people' feel powerless to do much with government.  I know with the horse here that she communicates on a higher plane because I respond to even her little signs.  Similarly, people should mature with a more responsive government.`);
-
-var Notaera3 = document.createTextNode(`
-I would also like to begin transition of 'voting on bills' so the people can have more government choice.  Bills get filled with unrelated things when they should be clear and concise single changes and the minimum related necessary changes to allow that single change to flow smoothly.  With internet communication, I would eventually prefer bills to reach the point of being clearly understandable to public readers, and voteable in ways such as a popup on social media saying 'to receive your basic income this month, select yes or no on this bill'.`);
-
-var Notaera4 = document.createTextNode(`
-I believe the 'transient' mindset should be dealt with.  This doesn't just refer to the homeless and poorest, but renting in general.  i mean how humans rarely make places better individually in any longterm noticeable way because most would be increasing someone elses property value with their own labor.  So instead of growing better, things slowly deteriorate, and while hidden homes can be nice, the visible public of humanity is somewhere humans generally are ok with having bad footprints individually.  In general the feeling of being an impermanent part of someone elses land isn't a good feeling and that's a sign it needs to eventually change.`);
 
 var Notaera5 = document.createTextNode(`
-another harder to solve thing to eventually solve anyway is money.  my songs are what i make them because im not making them for money.  i can make things high quality and creative because i'm not weighing everything by cost and net profit.  the constant sliding slope of having to pay for the neccessities of life and get the income somehow pushes humans onto paths they don't enjoy and in general their unhappiness leaks into the atmosphere of the emotional state of living creatures.  Some people have passions that are visible ~someone local likes to cook.  But a side issue with potential freedom to choose activities is how the free market is like the wild west for 'taking peoples money however possible'.  videogames and other leisure activities try to use whatever psychological tools they can to addict people.  They are in a constant struggle for continued influx of money with the others struggling around them.  Some local guy asked me 'so you have 72 hours spent on the song already and the money you are planning to make from that 72 hours is zero because you arent making it for humans?' and he just couldn't understand doing things people view as work without it generating money.  And how can I make a song for a bird or a horse or a dog with no human target audience and no editing to make it sellable?  But totally epic people should be able to happen ~and if my passion is making songs for horses, I should be able to.`);
-
-var Notaera6 = document.createTextNode(`
-So anyway, yeah I'm showoffy, but not really since really im bad at all the things you are great at! :) I could make us each an html theme and make us a website.  I don't use website makers and instead code the html and javascript myself to maintain power to do anything.  And since I dabble in node.js, we can have real servers connected doing servery things.  Anyway, I think a good starting point is seeing who has what passions so we can find highest potential ways to combine everyone.  Then figure out all the bases we need to cover.  LOVE U! Toodaloooooooo!
-like how my page title switches and acts as a mantra empowering good personality attributes?  i wuv it :)
+Meow. Start of ze great notaera site. calling it brainstorm central atm.  so I obviously need my stuff streamable and purchaseable in nicelooking forms that generate real income and spread.  So Spotify API is on the learn-list.  Symbiosis with a friend also in need of spotify on website.
 `);
-Notaeram.appendChild(Notaera);
-Notaeram2.appendChild(Notaera2);
-Notaeram3.appendChild(Notaera3);
-Notaeram4.appendChild(Notaera4);
+var Notaera6 = document.createTextNode(`
+The switchy title is fun.  it actually messes with SEO ~which I find silly.  I am thinking twitter api for a companion twitterbot would be good to learn too.
+`);
 Notaeram5.appendChild(Notaera5);
 Notaeram6.appendChild(Notaera6);
 var NotaeraMohka = document.getElementById("NotaeraMo");
-NotaeraMohka.appendChild(Notaeram);
-NotaeraMohka.appendChild(Notaeram2);
-NotaeraMohka.appendChild(Notaeram3);
-NotaeraMohka.appendChild(Notaeram4);
 NotaeraMohka.appendChild(Notaeram5);
 NotaeraMohka.appendChild(Notaeram6);
-	var Notaerav = document.createElement("p");
-var Notaerava = document.createTextNode(`click the two imgs to the left to hear songs.  i have 90trillion songs and some with much more progress and niceness than these BUT these are very anthemable and proud.  the nonpiano one is just starting, but the lyrics i hear in it are 'free forever, from sea to sea...' and that's pretty patriotic.  Would make commercials the kids run to the tv like 'oh yeah this commercial again!' and love the music.`);
-Notaerav.appendChild(Notaerava);
-var Notaeravere = document.getElementById("Notaeravere");
-Notaeravere.appendChild(Notaerav);
 }
 
 
@@ -94,81 +61,3 @@ function maaTani(){
    Taniklo = TaniTaera.split("@");
 }
 //Lotani
-
-//seallcorsa
-function seallcorsamohka(corsamohkaji){
-	corsamohkaji = corsamohkaji + "mohka.jpg";
-	document.getElementById("BackgroundMohka").src = corsamohkaji;
-	
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0.9";	
-}, 60);
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0.8";	
-}, 120);
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0.7";	
-}, 180);
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0.6";	
-}, 240);
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0.5";	
-}, 320);
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0.4";	
-}, 480);
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0.3";	
-}, 540);
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0.2";	
-}, 600);
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0.1";	
-}, 660);
-	setTimeout(function(){
-    document.getElementById("Background").style.opacity = "0";	
-}, 720);
-	setTimeout(function(){
-    document.getElementById("Background").src = corsamohkaji;
-	document.getElementById("Background").style.opacity = "1.0";	
-}, 720);
-	
-}
-//seallcorsa
-
-
-
-//vere
-function vere(verezel){
-        vereshh();
-        var vereli = document.getElementById("vere" + verezel);
-		vereli.play();
-		
-		seallcorsamohka(verezel);
-		maamohka(verezel);
-}
-
-function vereshh(){
-	var elements = document.getElementsByTagName('audio');
-
-for (var i = 0; i < elements.length; i++) {
-    elements[i].pause();
-	elements[i].currentTime = 0;
-}
-}
-//vere
-
-function maamohka(zel){
-	
-}
-
-
-function BeteverePlay(keycode){
-	if (keycode == 96){
-		BetevereAsk();
-}}
-function BetevereAsk(){
-
-	}
